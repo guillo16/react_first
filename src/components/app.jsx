7f19e5316e58429e52dcded4f10947f0b1101689
guillermo varela <guillovarela16@gmail.com>
2020-07-08 11:45:20 -0300

    Finish this part

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 import SearchBar from './search_bar.jsx';
 import Gift from './gift.jsx';
+import GifList from './gift_list.jsx';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+     gifs: [],
+     selectedGifId: "YRiEYJFPHEzPbHECfh"
+
+    }
+  }
   render() {
+    const gifs = [
+    {id: 'jUL2BawurnYHS6gSJ5'},
+    {id: 'YRiEYJFPHEzPbHECfh'}
+    ]
     return (
       <div>
         <div className="left-scene">
@@ -14,6 +27,7 @@ class App extends Component {
           </div>
         </div>
         <div className="right-scene">
+        <GifList gifs={gifs}/>
         </div>
       </div>
       )
