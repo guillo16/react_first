0d17f0a364c2c31faad2aaf4f15ded0b256b4dfb
guillermo varela <guillovarela16@gmail.com>
2020-07-15 12:00:39 -0300

    Finish something

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import SearchBar from './search_bar.jsx';
 import Gif from './gif.jsx';
 import GifList from './gif_list.jsx';
+import giphy from 'giphy-api';
 
 
 class App extends Component {
@@ -14,26 +15,30 @@ class App extends Component {
   }
 
   search = (query) => {
+    giphy('tE53IfrkhhbZ44RMXHsj1YAv1zyJv8Gf').search({
+    q: query,
+    rating: 'g',
+    limit: 10
+      }, (err, res) => {
+    // Res contains gif data!
+    this.setState({
+      gifs: res.data
+    })
+
+    });
 
   }
   render() {
-
-    const gifs = [
-      { id: "26BRv0ThflsHCqDrG" },
-      { id: "frB0ofQo5E0HZ5ZHwF" }
-    ];
     return(
       <div>
         <div className="left-scene">
-        <SearchBar />
+        <SearchBar searchFunction={this.search}/>
         <div className="selected-gif">
           <Gif id={this.state.selectedGifId}/>
         </div>
         </div>
         <div className="right-scene">
-
             <GifList gifs={this.state.gifs}/>
-
         </div>
       </div>
       )
--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   handleUpdate = (event) => {
-    console.log(event.currentTarget.value)
+    this.props.searchFunction(event.target.value)
   }
   render() {
     return(
