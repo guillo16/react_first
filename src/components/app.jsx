4ea04526a8de43186732f715d45c6310684645a8
guillermo varela <guillovarela16@gmail.com>
2020-07-22 18:21:42 -0300

    Finish same lesson show

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,17 +1,44 @@
 import React, { Component } from 'react';
 import SearchBar from './search_bar.jsx';
 import Gif from './gif.jsx';
+import GifList from './gif_list.jsx';
+import giphy from 'giphy-api';
 
 
 class App extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        gifs: [],
+        selectedGif: 'l0MYsHhzxfLsYfpPW'
+      }
+    }
+
+
+
+    search = (query) => {
+      giphy('tE53IfrkhhbZ44RMXHsj1YAv1zyJv8Gf').search({
+        q: query,
+        rating: 'g',
+        limit: 10
+      }, (err, res) => {
+         this.setState({
+          gifs: res.data
+         })
+      })
+
+
+    }
+
    render() {
     return(
       <div>
         <div className="left-scene">
-          <SearchBar />
-          <Gif id="l0MYsHhzxfLsYfpPW"/>
+          <SearchBar searchFunction={this.search}/>
+          <Gif id={this.state.selectedGif}/>
         </div>
         <div className="right-scene">
+            <GifList gifs={this.state.gifs}/>
         </div>
       </div>
       )
